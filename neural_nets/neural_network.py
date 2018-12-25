f8b23e930fe7b9af01118f225fbcefce84afe6e6
Nikolas Pitsillos <npitsillos@gmail.com>
2018-12-25 19:53:04 +0000

    Implemented weight initialisation

--- neural_nets/neural_network.py
+++ neural_nets/neural_network.py
@@ -16,7 +16,8 @@ class NeuralNetwork():
         """
             Constructs a Neural Network
 
-            neuron_list: defines number of neurons in each layer, size is # layers except input
+            neuron_list: defines number of neurons in each layer
+                         size is # layers except input
         """
         self.num_layers = len(neuron_list)
         self.layers = []
@@ -25,6 +26,45 @@ class NeuralNetwork():
         for neurons in neuron_list:
             layer = NeuralLayer(neurons, sigmoid)
             self.layers.append(layer)
+    
+    def initialise_weights(self, num_inputs):
+        """
+            Initialises weights of every neuron in the 
+            network
+
+            num_inputs: number of initial inputs to the network
+        """
+
+        # Each neuron's inputs in a layer is the number of neurons in
+        # the previous layer.  The number of neurons in the input layer
+        # is the number of inputs.
+        previous_layer_neurons = num_inputs
+
+        for layer in self.layers:
+            for neuron in layer.neurons:
+                neuron.weights = np.random.normal(0,1, previous_layer_neurons)
+            previous_layer_neurons = layer.num_neurons
+    
+    def feed_forward(self, inputs):
+        """
+            Forward propagate the inputs to this network
+
+            inputs: inputs to this Network
+        """
+        self.outputs = []
+        
+        # input neurons can be thought of as a layer that
+        # just propagates the input values
+        layer_outputs = inputs
+
+        # Propagate layer outputs until final hidden layer
+        for i in range(self.num_layers-1):
+            layer_outputs = (self.layers[i].feed_forward(layer_outputs))
+        
+        # Now propagate though output layer
+        self.outputs = self.layers[self.num_layers-1].feed_forward(layer_outputs)
+
+        return self.outputs
         
     def inspect(self):
         """
